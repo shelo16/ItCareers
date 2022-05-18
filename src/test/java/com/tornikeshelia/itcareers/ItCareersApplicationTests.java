463aefb9b471d334ae49906305a8539b1af0b5ff
Tornike Shelia <tornike_shelia@epam.com>
2022-05-18 22:21:29 +0400

    first commit

---
+++ src/test/java/com/tornikeshelia/itcareers/ItCareersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tornikeshelia.itcareers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItCareersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
